3742420be14cf399c18f1aa81e4334a0662c1e7a
czgao <15927274250@163.com>
2018-12-02 15:45:40 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <img alt="Vue logo" src="./assets/1.jpg">
+    <img alt="Vue logo" src="./assets/2.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
