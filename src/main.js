92e136228eb75b8b3edcf2530f89f6cebee5f6f6
“yo‘‘czgao’ <l>
2019-02-26 17:50:00 +0800

    模板

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+{{#if_eq library "element"}}
+console.log('element')
+{{/if_eq}}
 new Vue({
   render: h => h(App)
 }).$mount('#app')
